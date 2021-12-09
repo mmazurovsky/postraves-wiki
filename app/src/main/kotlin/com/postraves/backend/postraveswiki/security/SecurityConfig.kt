1f2bf1c95a628a4549bd2142f4a4dbbe34b7a437
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2021-12-09 17:42:52 +0300

    - removed firebase hosting origin from allowed origins cors

--- app/src/main/kotlin/com/postraves/backend/postraveswiki/security/SecurityConfig.kt
+++ app/src/main/kotlin/com/postraves/backend/postraveswiki/security/SecurityConfig.kt
@@ -53,7 +53,7 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val config = CorsConfiguration()
-        config.allowedOriginPatterns = listOf("https://*.postraves.com", "http://localhost:49257", "https://localhost:49257", "https://postraves.web.app")
+        config.allowedOriginPatterns = listOf("https://*.postraves.com", "http://localhost:49257", "https://localhost:49257")
         config.allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         config.applyPermitDefaultValues()
         val source = UrlBasedCorsConfigurationSource()
