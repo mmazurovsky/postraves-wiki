a9041aff12deedd900b7e8c4adf8dde62188c6b5
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2021-12-11 22:59:07 +0300

    - substituted hard coded images in docker compose files with env variables - added deploy prod script - changed deploy dev script - added eb config

---
+++ deploy-prod.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker buildx build --platform=linux/amd64 -t mmazurovsky/postraves-nginx-prod ./nginx --no-cache
+docker push mmazurovsky/postraves-nginx-prod
+docker-compose -f ./app/docker-compose-api-prod.yml build
+docker push mmazurovsky/postraves-wiki-api-prod
+eb deploy
