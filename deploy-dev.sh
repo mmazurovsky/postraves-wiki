8d4b5982861c4c1bccdb5e0591062a0ba1dac7d8
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2021-12-11 23:57:02 +0300

    - updated deploy scripts and gitignore

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,6 +2,6 @@
 
 docker buildx build --platform=linux/amd64 -t mmazurovsky/postraves-nginx ./nginx --no-cache
 docker push mmazurovsky/postraves-nginx
-docker-compose -f ./app/docker-compose-api.yml build
+docker-compose -f ./app/docker-compose-api.yml --env-file .env-dev build
 docker push mmazurovsky/postraves-wiki-api
 eb deploy
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,6 +2,6 @@
 
 docker buildx build --platform=linux/amd64 -t mmazurovsky/postraves-nginx-prod ./nginx --no-cache
 docker push mmazurovsky/postraves-nginx-prod
-docker-compose -f ./app/docker-compose-api-prod.yml build
+docker-compose -f ./app/docker-compose-api-prod.yml --env-file .env-prod build
 docker push mmazurovsky/postraves-wiki-api-prod
 eb deploy
