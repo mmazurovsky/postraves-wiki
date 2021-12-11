4cb89e2409479322f7cd625144d775a7c8e5d5f9
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2021-12-11 17:38:38 +0300

    - added flyway to docker-compose for aws - added deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#docker buildx build --platform=linux/amd64 -t mmazurovsky/postraves-nginx ./nginx --no-cache
+#docker push mmazurovsky/postraves-nginx
+#docker-compose -f ./app/docker-compose-api.yml build
+#docker push mmazurovsky/postraves-wiki-api
+eb deploy
