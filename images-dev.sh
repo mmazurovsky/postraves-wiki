7bf19b0f515df1c242c13fbf3876e785554f92ab
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2022-01-31 15:28:37 +0300

    - added ssl configuration to nginx - made docker compose file abstract depending on env variables with names of images and locations of env files - removed aws configs

--- images-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-docker buildx build --platform=linux/amd64 -t registry.digitalocean.com/postraves-registry/postraves-nginx-dev ./nginx --no-cache
-docker push registry.digitalocean.com/postraves-registry/postraves-nginx-dev --all-tags
-docker-compose -f ./app/docker-compose-api-dev.yml build
-docker push registry.digitalocean.com/postraves-registry/postraves-wiki-api-dev
